dc8a5d1afa6ea0aa0ac76329e1510f4ce44994f4
Дмитрий <bazaley-dmitriy@i.ua>
2022-11-04 23:57:58 +0200

    open-modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 // import { API_KEY, URL } from './js/fetch';
+
+import './js/open-modal';
---
+++ src/js/open-modal.js
@@ -0,0 +1,18 @@
+// import { renderCard } from './render-card';
+// import {fetchMovieById} from 'fetchMoviesById'
+
+const filmCardsRef = document.querySelector('.film-cards');
+const backdropRef = document.querySelector('.backdrop');
+
+filmCardsRef.addEventListener('click', onFilmCardsClick);
+
+function onFilmCardsClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName === 'IMG') {
+    const movieId = e.target.dataset.id;
+    backdropRef.classList.remove('is-hidden');
+    const movie = fetchMovieById(movieId);
+    renderCard(movie);
+  }
+}
