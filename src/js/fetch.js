210e4cb8df9efd2f74f3c7c240628b4c0dd3ad63
Helen <helenlena52@gmail.com>
2022-11-04 08:09:35 +0200

    add-url

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,4 +1,6 @@
 import { movies } from './test-api';
 
 const API_KEY = '341a3443d9168179889e844c5b12f774';
-const URL = 'https://api.themoviedb.org/3/search/movie';
+const URL_TREND = 'https://api.themoviedb.org/3/trending/all/day';
+const URL_QUERY = 'https://api.themoviedb.org/3/search/movie';
+const URL_ID = `https://api.themoviedb.org/3/movie/${movie_id}`;
