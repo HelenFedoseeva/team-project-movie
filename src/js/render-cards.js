5ced02e2046b13ba9db06b24bd2ad2b7fae303b4
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-11-06 00:54:10 +0100

    Update render-cards.js

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -11,9 +11,9 @@ function createCards(movies) {
   const { results } = movies;
   return results
     .map(
-      ({ poster_path, title, release_date }) =>
+      ({ poster_path, title, release_date, id }) =>
         `<li class="movie-card">
-      <img src="${IMAGE_URL}${poster_path}" alt="${title} class="movie-card-img""/>
+      <img src="${IMAGE_URL}${poster_path}" alt="${title} class="movie-card-img" data-id='${id}'"/>
       <div class="movie-info">
       <p class="movie-title">${title}</p>
       <p class="film-cards__title movie-genres">Жанры когда будут | ${release_date.slice(
