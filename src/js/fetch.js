12cfcad71b9f4f39f3eda619cee5c10c1edae54e
Helen <helenlena52@gmail.com>
2022-11-05 09:47:34 +0200

    add-div-to-render-cards

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,4 +3,4 @@ import { movies } from './test-api';
 const API_KEY = '341a3443d9168179889e844c5b12f774';
 const URL_TREND = 'https://api.themoviedb.org/3/trending/all/day';
 const URL_QUERY = 'https://api.themoviedb.org/3/search/movie';
-const URL_ID = `https://api.themoviedb.org/3/movie/${movie_id}`;
+// const URL_ID = `https://api.themoviedb.org/3/movie/${movie_id}`;
--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -15,11 +15,13 @@ function createCards() {
       ({ poster_path, title, release_date }) =>
         `<li class="movie-card">
       <img src="${IMAGE_URL}${poster_path}" alt="${title} class="movie-card-img""/>
+      <div class="movie-info">
       <p class="movie-title">${title}</p>
       <p class="film-cards__title">Жанры когда будут| ${release_date.slice(
         0,
         4
-      )}</p>
+      )}</p> 
+      </div>
     </li>`
     )
     .join('');
