a9e57c1dff08a18fea852ae3973c5e23b9235c51
Helen <helenlena52@gmail.com>
2022-11-06 09:29:12 +0200

    add correct index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { FetchMovie } from './js/fetch';
+import { fechMuviQueri } from './js/fetch';
+import { renderCards } from './js/render-cards';
+// import { API_KEY, URL } from './js/fetch';
+import './js/open-modal';
+import './js/moviesBySubmit';
+import './js/renderPagination';
+import './js/adding-watched-movies';
