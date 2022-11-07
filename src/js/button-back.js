520be9459537119dceb727c8acbe7476137f88a0
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-11-07 18:10:26 +0100

    fixed render and genre
    поправил функции, добавил жанры на карточки, включил кнопку назад.

--- src/js/button-back.js
+++ src/js/button-back.js
@@ -5,7 +5,6 @@ const buttonEl = document.querySelector('.button-back');
 document.addEventListener('scroll', throttle(onWindowScroll, 400));
 
 function onWindowScroll(e) {
-  console.log(window.scrollY);
   if (window.scrollY >= 700) {
     buttonEl.classList.remove('hidden');
     buttonEl.addEventListener('click', onButtonClick);
@@ -16,7 +15,6 @@ function onWindowScroll(e) {
 }
 
 function onButtonClick(e) {
-  console.log(e);
   window.scrollTo({
     top: 0,
     left: 0,
