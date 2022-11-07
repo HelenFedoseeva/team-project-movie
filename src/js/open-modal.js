f66438a068778daf7e03450a89d863fe1d9b35e8
Svetlana Redyukhina <svetikred77@gmail.com>
2022-11-07 19:19:18 +0200

    add-movies

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,21 +1,21 @@
 import { modalCardRender } from './modalCardRender';
 import { getMovie } from './movieById';
-// import { addListenerByBtns } from './adding-watched-movies';
+import { addListenerByBtns } from './adding-watched-movies';
 
 const filmCardsRef = document.querySelector('.gallery');
 const backdropRef = document.querySelector('.backdrop');
-const modalRef = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector(".modal-btn");
+const modalRef = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.modal-btn');
 
 filmCardsRef.addEventListener('click', onFilmCardsClick);
 
 function onFilmCardsClick(e) {
   e.preventDefault();
 
-  document.addEventListener("keydown", onEscTap);
-  backdropRef.addEventListener("click", onClickCloseBtnOrOutside);
-  modalCloseBtn.addEventListener("click", onClickCloseBtnOrOutside);
-  
+  document.addEventListener('keydown', onEscTap);
+  backdropRef.addEventListener('click', onClickCloseBtnOrOutside);
+  modalCloseBtn.addEventListener('click', onClickCloseBtnOrOutside);
+
   if (e.target.nodeName === 'IMG') {
     const movieId = e.target.dataset.id;
 
@@ -29,30 +29,28 @@ async function openModal(movieId) {
   const movie = await getMovie(movieId);
 
   modalCardRender(movie.data);
-  // addListenerByBtns(movie);
+  addListenerByBtns(movie);
 }
 
 function onEscTap(e) {
-    
-    if (e.key === "Escape") {
-
-        closeModal()
-    };
-};
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+}
 
 function onClickCloseBtnOrOutside(e) {
-
-  if (e.currentTarget.classList.value === "modal-btn" || e.target.classList.value === "backdrop") {
-      
-        closeModal();
-    };  
-};
+  if (
+    e.currentTarget.classList.value === 'modal-btn' ||
+    e.target.classList.value === 'backdrop'
+  ) {
+    closeModal();
+  }
+}
 
 function closeModal() {
-
-    backdropRef.classList.toggle("is-hidden");
-    document.removeEventListener("keydown", onEscTap);
-    backdropRef.removeEventListener("click", onClickCloseBtnOrOutside);
-    const wrapper = document.querySelector(".wrapper");
-    wrapper.remove();
-};
+  backdropRef.classList.toggle('is-hidden');
+  document.removeEventListener('keydown', onEscTap);
+  backdropRef.removeEventListener('click', onClickCloseBtnOrOutside);
+  const wrapper = document.querySelector('.wrapper');
+  wrapper.remove();
+}
