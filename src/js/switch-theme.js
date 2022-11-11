30679127b419b6853d81ef37002d1a9c23193ee2
Helen <helenlena52@gmail.com>
2022-11-11 20:02:44 +0200

    Update switch-theme.js

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -8,9 +8,9 @@ swithThemeBtn.addEventListener('click', switchToDarkTheme);
 
 function switchToDarkTheme() {
   bodyRef.classList.toggle('dark-theme');
-  const cardsMovieRef = document.querySelectorAll('.movie-info');
-  const headerRef = document.querySelector('.header');
-  headerRef.classList.toggle('header-dark');
+  // const cardsMovieRef = document.querySelectorAll('.movie-info');
+  // const headerRef = document.querySelector('.header');
+  // headerRef.classList.toggle('header-dark');
   footerRef.classList.toggle('footer__dark');
   footerTextRef.classList.toggle('footer-text__dark');
   footerLinkRef.classList.toggle('footer-link__dark');
